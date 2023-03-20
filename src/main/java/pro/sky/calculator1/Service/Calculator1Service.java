861c006b6baa28d7a5c3e397b1a258401ddfb39d
volynkina <bzzz18@yandex.ru>
2023-03-20 17:14:22 +0500

    check

---
+++ src/main/java/pro/sky/calculator1/Service/Calculator1Service.java
@@ -0,0 +1,7 @@
+package pro.sky.calculator1.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator1Service {
+}
---
+++ src/main/java/pro/sky/calculator1/controller/calculator1Controller.java
@@ -0,0 +1,7 @@
+package pro.sky.calculator1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class calculator1Controller {
+}
